46bd4fbf3879ddb967ec10c68d5f7db2b302866b
paula dixon <paulapdixon@gmail.com>
2021-03-23 12:48:07 -0700

    adding notes on data types and references

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 // all valid js is valid ts
-console.log('sup hi');
+var _this = this;
+// notes on void
+function logToConsole() {
+    console.log('hi');
+    return;
+}
+// => returns void
+// notes on array syntax
+var fruits;
+var food;
+var genericArray;
+var genTuple;
+// notes on object syntax
+var user = {
+    firstName: 'paula',
+    lastName: 'dixon',
+    username: 'pauladixon',
+    getUsername: function () { return _this.username; }
+};
+// enums are another way to organize data
+var DaysOfTheWeek;
+(function (DaysOfTheWeek) {
+    DaysOfTheWeek["MONDAY"] = "monday";
+    DaysOfTheWeek[DaysOfTheWeek["TUESDAY"] = void 0] = "TUESDAY";
+    DaysOfTheWeek[DaysOfTheWeek["WEDNESDAY"] = void 0] = "WEDNESDAY";
+})(DaysOfTheWeek || (DaysOfTheWeek = {}));
+var dayToday;
+dayToday = DaysOfTheWeek.MONDAY;
+var Continents;
+(function (Continents) {
+    Continents[Continents["North_America"] = 0] = "North_America";
+    Continents[Continents["South_America"] = 1] = "South_America";
+    Continents[Continents["Africa"] = 2] = "Africa";
+    Continents[Continents["Asia"] = 3] = "Asia";
+    Continents[Continents["Europe"] = 4] = "Europe";
+    Continents[Continents["Antartica"] = 5] = "Antartica";
+    Continents[Continents["Australia"] = 6] = "Australia";
+})(Continents || (Continents = {}));
+// usage
+var region = Continents.Africa;
+// if you're not ready to specify a type, you can use any
+var authorizedUser, _a = void 0,  = _a.firstName,  = _a["paula"],  = _a.lastName,  = _a["dixon"];
+// but this lacks intellisense as opposed to:
+var authorizedUser = {
+    firstName: 'paula',
+    lastName: 'dixon'
+};
--- index.ts
+++ index.ts
@@ -1,4 +1,68 @@
 // all valid js is valid ts
 
+// notes on void
 
-console.log('sup hi')
+    function logToConsole(): void {
+        console.log('hi')
+        return
+    }
+
+    // => returns void
+
+// notes on array syntax
+
+    let fruits: string[]
+    let food: Array<string>
+    let genericArray: Array<string | number | boolean>
+    let genTuple: [number, string, boolean]
+
+// notes on object syntax
+
+    let user = {
+        firstName: 'paula',
+        lastName: 'dixon',
+        username: 'pauladixon',
+        getUsername: (): string => this.username
+    }
+
+// enums are another way to organize data
+    
+    enum DaysOfTheWeek {
+        MONDAY = 'monday',
+        TUESDAY, 
+        WEDNESDAY
+    }
+
+    let dayToday: DaysOfTheWeek
+    dayToday = DaysOfTheWeek.MONDAY
+
+    enum Continents {
+        North_America,
+        South_America,
+        Africa,
+        Asia,
+        Europe,
+        Antartica,
+        Australia
+    }
+    
+    // usage
+    let region = Continents.Africa;
+
+
+// if you're not ready to specify a type, you can use any
+
+    let authorizedUser: any {
+        firstName: 'paula',
+        lastName: 'dixon',
+    }
+
+    // but this lacks intellisense as opposed to:
+    
+    let authorizedUser: {
+        firstName: string,
+        lastName: string,
+    } = {
+        firstName: 'paula',
+        lastName: 'dixon'
+    }
